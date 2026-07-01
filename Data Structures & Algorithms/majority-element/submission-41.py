a9658bf0a397f18ac57ea450a781a53407ad4db7
Abdelkerim Mahamat Habib Doutoum <doutoumabdelkerim90@gmail.com>
2026-07-01 22:11:55 +0400

    Add: majority-element - submission-41

---
+++ Data Structures & Algorithms/majority-element/submission-41.py
@@ -0,0 +1,7 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+        for i in range(len(nums)):
+            count[nums[i]] = count.get(nums[i], 0) + 1
+            if count[nums[i]] > len(nums) / 2:
+                return nums[i]
