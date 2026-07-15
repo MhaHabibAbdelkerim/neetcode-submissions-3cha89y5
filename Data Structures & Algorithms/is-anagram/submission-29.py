dd5676cd105ef8b3ba07cfe6b57724ffd9b42f2a
Abdelkerim Mahamat Habib Doutoum <doutoumabdelkerim90@gmail.com>
2026-07-15 16:34:01 +0400

    Add: is-anagram - submission-29

---
+++ Data Structures & Algorithms/is-anagram/submission-29.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+          return False
+        
+        CounterS, CounterT = {}, {}
+        for i in range(len(s)):
+          CounterS[s[i]] = 1 + CounterS.get(s[i], 0)
+          CounterT[t[i]] = 1 + CounterT.get(t[i], 0)
+
+        return CounterS == CounterT
