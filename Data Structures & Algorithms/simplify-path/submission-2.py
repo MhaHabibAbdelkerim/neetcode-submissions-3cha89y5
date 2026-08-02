66a77ca20f0dae3e04b7c66f41f22499960b4894
Abdelkerim Mahamat Habib Doutoum <doutoumabdelkerim90@gmail.com>
2026-08-02 22:03:41 +0400

    Add: simplify-path - submission-2

---
+++ Data Structures & Algorithms/simplify-path/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        current_directory = ""
+        for c in path + "/":
+            if c == "/":
+                if current_directory == "..":
+                    if stack: stack.pop()
+                elif current_directory != "" and current_directory != ".":
+                    stack.append(current_directory)
+                current_directory = ""
+            else:
+                current_directory += c
+
+        return "/" + "/".join(stack)
