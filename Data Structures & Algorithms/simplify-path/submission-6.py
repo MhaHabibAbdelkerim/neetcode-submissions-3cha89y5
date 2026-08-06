cc73396d26cc84455d3fd313b9aa10c17b0128a3
Abdelkerim Mahamat Habib Doutoum <doutoumabdelkerim90@gmail.com>
2026-08-06 21:42:11 +0400

    Add: simplify-path - submission-6

---
+++ Data Structures & Algorithms/simplify-path/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        current_path = ""
+        stack = []
+
+        for character in path + "/":
+            if character == "/":
+                if current_path == "..":
+                    if stack: stack.pop()
+                elif current_path != "." and current_path != "":
+                    stack.append(current_path)
+                current_path = ""
+            else:
+                current_path += character
+
+        return "/" + "/".join(stack)
